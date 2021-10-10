bf39911b1ba7a6af2d55d7f9053cee43db3f2cc6
eisforgene <82473195+eisforgene@users.noreply.github.com>
2021-10-10 12:13:13 -0700

    setup User.js and api-routes

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    trim: true,
+    required: 'Username is Required'
+  },
+
+  email: {
+    type: String,
+    unique: true,
+    match: [/.+@.+\..+/]
+  },
+
+  thought: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Thought'
+  }],
+
+  friends: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
--- server.js
+++ server.js
@@ -9,11 +9,12 @@ const db = require('./models');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/populatedb', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network', {
   useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
-mongoose.set('useCreateIndex', true);
-mongoose.set('debug', true);
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
