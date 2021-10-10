f583d01cdce2dda322fcf22543624fc67131dd81
eisforgene <82473195+eisforgene@users.noreply.github.com>
2021-10-10 12:42:54 -0700

    completed api routes

--- router/api-routes.js
+++ router/api-routes.js
@@ -1,55 +1,59 @@
 const app = require('express').Router();
 let User = require('../models/User');
 
-User.create({ name: 'Ernest Hemingway' })
-  .then(dbUser => {
-    console.log(dbUser);
-  })
-  .catch(({ message }) => {
-    console.log(message);
-  });
-
 app.get('/api/users', (req, res) => {
-  User.find({})
-    .then(dbUser => {
-      res.json(dbUser);
-    })
-    .catch(err => {
-      res.json(err);
-    }); 
+    User.find({})
+        .then(dbUser => {
+            res.json(dbUser);
+        })
+        .catch(err => {
+            res.json(err);
+        });
 });
 
 app.get('/api/users/:id', (req, res) => {
-  User.findById(req.params.id)
-    .then(dbUser => {
-      res.json(dbUser);
-    })
-    .catch(err => {
-      res.json(err);
-    });
+    User.findById(req.params.id)
+        .then(dbUser => {
+            res.json(dbUser);
+        })
+        .catch(err => {
+            res.json(err);
+        });
 });
 
 app.post('/api/users', ({ body }, res) => {
-  User.create(body)
-    .then(dbUser => {
-      res.json(dbUser);
-    })
-    .catch(err => {
-      res.json(err);
-    });
+    User.create(body)
+        .then(dbUser => {
+            res.json(dbUser);
+        })
+        .catch(err => {
+            res.json(err);
+        });
 });
 
-app.get('/populate', (req, res) => {
-  User.find({})
-    .populate({
-      path: 'notes',
-      select: '-__v'
-    })
-    .select('-__v')
-    .then(dbUser => {
-      res.json(dbUser);
-    })
-    .catch(err => {
-      res.json(err);
-    });
+app.put('/api/users/:id', (req, res) => {
+    User.findByIdAndUpdate(req.params.id, { $set: req.body })
+        .populate({
+            path: 'notes',
+            select: '-__v'
+        })
+        .select('-__v')
+        .then(dbUser => {
+            res.json(dbUser);
+        })
+        .catch(err => {
+            res.json(err);
+        });
 });
+
+app.delete('/api/users/:id', (req, res) => {
+    User.destroy({ _id: req.params.id })
+        .then(dbUser => {
+            console.log(dbUser);
+        })
+        .catch(({ message }) => {
+            console.log(message);
+        });
+})
+
+module.exports = app;
